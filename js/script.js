806ae2008282a64a0c0d1c60567203fd3cf4020c
Facundo Trotta <keygen.lol.dc11@gmail.com>
2025-02-26 21:21:26 -0300

    ultimos cambios para entregar

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
 //saludo
-const pedirNombre = prompt ("Ingrese su nombre") ;
-const saludar = ("hola "+ pedirNombre);
-
-alert (saludar);
-console.log (saludar);
-
+alert ("hola y encantado de que vengas a nuestras a nuestras pagina");
+function saludar (nombre) {
+    console.log ("hola y bienvenido" + nombre);
+    alert ("hola y bienvenido " + nombre);
+}
+function pedirNombre () {
+    let nombre = prompt ("¿como te llamas?");
+    saludar (nombre);
+}
+pedirNombre ();
 //preguntas con ciclos 
 alert ("Estamos en la duda de marca de ropa con la que trabajar primero, asi que lo dejaremos al publico.");
 let respuesta ; 
@@ -24,10 +28,22 @@ do {
     }
 } while (respuesta !== "si" && respuesta !== "no");
 
-
+//un poco de arrays
+let ropa = ["blanco","negro"]
+let preguntaRopa = prompt ("¿que color de ropa te gusta mas? blanco o negro")
+    if (preguntaRopa === ropa[0]) {
+        console.log ("hermoso color de ropa");
+        alert ("hermoso color de ropa");
+    }else if (preguntaRopa === ropa[1]) {
+        console.log ("hermoso color de ropa");
+        alert ("hermoso color de ropa");
+    }else { 
+        console.log ("disculpe por las molestias y gracias por pasar");
+        alert ("disculpe por las molestias y gracias por pasar");
+    }
 
 //pregunta simple 
-let pregunta = prompt ("¿adidas o nike?")
+const pregunta = prompt ("¿adidas o nike?")
 if (pregunta === "adidas") {
     console.log ("Increible, trabajaremos para traer esa marcas primero");
     alert ("Increible, trabajaremos para traer esa marcas primero");
